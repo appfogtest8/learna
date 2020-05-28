b965513372910f6f0170d6ea16c2e6f7289ffdfb
app fogtest <appfogtest8@mm.st>
2020-05-28 16:48:58 -0700

    Ch 3: Exploring Activities

--- app/src/main/java/com/exampler/timefighter/MainActivity.kt
+++ app/src/main/java/com/exampler/timefighter/MainActivity.kt
@@ -2,10 +2,33 @@ package com.exampler.timefighter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var gameScoreTextView: TextView
+    private lateinit var timeLeftTextView: TextView
+    private lateinit var tapMeButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun  incrementScore() {
+
+    }
+
+    private fun resetGame() {
+
+    }
+
+    private fun startGame() {
+
+    }
+
+    private fun endGame() {
+        
     }
 }
